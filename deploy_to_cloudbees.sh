5e94eed42708252d719363fd18547057f7120ee4
Laurent Vaills <laurent.vaills@bonitasoft.com>
2013-11-02 18:51:35 +0100

    Use the maven-shade-plugin instead of packaging a zip file separately

--- deploy_to_cloudbees.sh
+++ deploy_to_cloudbees.sh
@@ -7,4 +7,5 @@
 APP_NAME=$1
 
 # Deploy APP_NAME application to cloudbees
-bees app:deploy -a $APP_NAME -t java -Rclass=org.nca.elevator.Server -Rjava_version=1.7 -Rargs='find $app_port ClassicStrategy' elevator.zip
+bees app:deploy -a $APP_NAME -t java -Rclass=org.nca.elevator.Server -Rjava_version=1.7 -Rargs='find $app_port ClassicStrategy' target/elevator-0.1.0-SNAPSHOT.jar
+
--- prepare_cloudbees.sh
+++
@@ -1,6 +0,0 @@
-# Generate zip file for cloudbees deployment
-mvn clean install
-cd target/classes
-zip -r ../../elevator.zip org/nca/*
-cd ../..
-zip -r -j elevator.zip target/deps/*.jar  
