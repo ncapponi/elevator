948da372f5158d3ce70bf3237644ca7b5b700bfc
Laurent Vaills <laurent.vaills@bonitasoft.com>
2013-11-04 20:02:44 +0100

    Reindented class WaitingUsers

--- src/main/java/org/nca/elevator/WaitingUsers.java
+++ src/main/java/org/nca/elevator/WaitingUsers.java
@@ -1,5 +1,7 @@
 package org.nca.elevator;
 
+import static java.lang.Math.abs;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -10,80 +12,80 @@ import org.slf4j.LoggerFactory;
 
 class WaitingUsers {
 
-	static final Logger logger = LoggerFactory.getLogger(WaitingUsers.class);
+    static final Logger logger = LoggerFactory.getLogger(WaitingUsers.class);
 
-	private List<WaitingUser> users = new ArrayList<WaitingUser>();
+    private List<WaitingUser> users = new ArrayList<WaitingUser>();
 
-  /**
-   * Receives a tick.
-   */
-  public void tick() {
-    for (WaitingUser user : users) {
-      user.tick();
+    /**
+     * Receives a tick.
+     */
+    public void tick() {
+        for (WaitingUser user : users) {
+            user.tick();
+        }
     }
-  }
 
-	public void add(WaitingUser user) {
-		users.add(user);
-	}
-
-	public WaitingUser popUser(int floor) {
-	  WaitingUser selectedUser = null;
-	  Iterator<WaitingUser> it = users.iterator();
-    while (it.hasNext()) {
-      WaitingUser user = it.next();
-      if (user.isWaitingAt(floor)) {
-			  selectedUser = user;
-			  it.remove();
-			}
-		}
-    if (selectedUser == null) {
-      logger.warn(
-          "Unable to find first waiting user for floor {}, providing one without direction", floor);
-      selectedUser = new WaitingUser(floor, Direction.UNKNOWN);
+    public void add(WaitingUser user) {
+        users.add(user);
     }
-    return selectedUser;
-	}
 
-  public int nbUsersToward(Direction direction, int currentFloor) {
-    int min = direction == Direction.UP ? currentFloor + 1 : 0;
-    int max = direction == Direction.UP ? Elevator.MAX_FLOOR : currentFloor - 1;
-    int count = 0;
-    for (int floor = min; floor <= max; floor++) {
-      count = count + nbUsersFor(floor);
+    public WaitingUser popUser(int floor) {
+        WaitingUser selectedUser = null;
+        Iterator<WaitingUser> it = users.iterator();
+        while (it.hasNext()) {
+            WaitingUser user = it.next();
+            if (user.isWaitingAt(floor)) {
+                selectedUser = user;
+                it.remove();
+            }
+        }
+        if (selectedUser == null) {
+            logger.warn(
+                    "Unable to find first waiting user for floor {}, providing one without direction", floor);
+            selectedUser = new WaitingUser(floor, Direction.UNKNOWN);
+        }
+        return selectedUser;
     }
-    return count;
-  }
 
-  public int scoreToward(Direction direction, int currentFloor) {
-    int min = direction == Direction.UP ? currentFloor + 1 : 0;
-    int max = direction == Direction.UP ? Elevator.MAX_FLOOR : currentFloor - 1;
-    int score = 0;
-    for (int floor = min; floor <= max; floor++) {
-      score += (Elevator.MAX_FLOOR - Math.abs(currentFloor - floor)) * nbUsersFor(floor);
+    public int nbUsersToward(Direction direction, int currentFloor) {
+        int min = direction == Direction.UP ? currentFloor + 1 : 0;
+        int max = direction == Direction.UP ? Elevator.MAX_FLOOR : currentFloor - 1;
+        int count = 0;
+        for (int floor = min; floor <= max; floor++) {
+            count += nbUsersFor(floor);
+        }
+        return count;
     }
-    return score;
-  }
 
-	public boolean hasUserToward(Direction direction, int currentFloor) {
-    return nbUsersToward(direction, currentFloor) > 0;
-  }
+    public int scoreToward(Direction direction, int currentFloor) {
+        int min = direction == Direction.UP ? currentFloor + 1 : 0;
+        int max = direction == Direction.UP ? Elevator.MAX_FLOOR : currentFloor - 1;
+        int score = 0;
+        for (int floor = min; floor <= max; floor++) {
+            score += (Elevator.MAX_FLOOR - abs(currentFloor - floor)) * nbUsersFor(floor);
+        }
+        return score;
+    }
 
-  public boolean hasUserFor(int floor) {
-		return nbUsersFor(floor) > 0;
-	}
+    public boolean hasUserToward(Direction direction, int currentFloor) {
+        return nbUsersToward(direction, currentFloor) > 0;
+    }
 
-	public int nbUsersFor(int floor) {
-		int number = 0;
-		for (WaitingUser user : users) {
-      if (user.isWaitingAt(floor))
-				number++;
-		}
-		return number;
-	}
+    public boolean hasUserFor(int floor) {
+        return nbUsersFor(floor) > 0;
+    }
 
-	@Override
-	public String toString() {
-    return "Wait: [" + users + "]";
-	}
+    public int nbUsersFor(int floor) {
+        int number = 0;
+        for (WaitingUser user : users) {
+            if (user.isWaitingAt(floor))
+                number++;
+        }
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Wait: [" + users + "]";
+    }
 }
